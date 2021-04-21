21486974b0dfcc515ed710a8756d29f595ade3f1
Pommers <richard@the-poms.com>
2021-04-21 07:52:22 +0100

    Add ptf archive configuration Amend name 'Pan-STARRS' Add 'R' filterSelection

--- src/LCExtract/config.py
+++ src/LCExtract/config.py
@@ -14,19 +14,22 @@ from LCExtract.utilities import Namespace
 
 Archive = collections.namedtuple('Archive', 'name code filters URL magField timeField filterField marker')
 
-panstarrs = Archive(name='PanSTARRS', code='p', filters='grizy',
+panstarrs = Archive(name='Pan-STARRS', code='p', filters='grizy',
                     URL='https://catalogs.mast.stsci.edu/api/v0.1/panstarrs',
                     magField='psfMag', timeField='obsTime', filterField='filtercode', marker='*')
 ztf = Archive(name='ZTF', code='z', filters='gri',
               URL='https://irsa.ipac.caltech.edu/cgi-bin/ZTF/',
               magField='mag', timeField='mjd', filterField='filterID', marker='.')
+ptf = Archive(name='PTF', code='r', filters='gR',
+              URL='https://irsa.ipac.caltech.edu/cgi-bin/Gator/',
+              magField='mag_autocorr', timeField='obsmjd', filterField='filterID', marker='+')
 
-archives = {'p': panstarrs, 'z': ztf}
+archives = {'p': panstarrs, 'z': ztf, 'r': ptf}
 archAvail = "".join(list(archives.keys()))
 
 # Global variables
 coneRadius = 1 / 3600  # 1 arcseconds
-filterSelection = 'grizy'
+filterSelection = 'grizyR'
 defaultFileName = 'data/test_new.csv'
 badResponse = (False, '')
 
